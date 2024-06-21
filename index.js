decae4df2f279f861ed45a74ce252fa27d499742
Asad Rana <asadlukman246@gmail.com>
2024-06-21 10:16:35 +0530

    added cors package

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors=require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const errorMiddleware=require("./middleware/error");
 const userRoutes=require("./routes/userRoutes");
 // Connect to MongoDB
+app.use(cors());
 app.use(bodyParser.json());
 mongoose.connect(process.env.DB_URI)
   .then(() => console.log('MongoDB Connected'))
