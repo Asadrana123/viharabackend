90176f24696568a107550f72eb61dc822d0dffd9
Asad Rana <asadlukman246@gmail.com>
2024-06-24 20:55:25 +0530

    modify model for product schema

--- model/productModel.js
+++ model/productModel.js
@@ -1,89 +1,51 @@
 const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
-    product_name: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    selller_id: {
-        type: Number,
-        default: 1,
-        required: true,
-    },
-    auction_type: {
-        type: String,
-        required: true,
-    },
-    product_type: {
-        type: Number,
-        required: true,
-    },
-    start_date: {
-        type: Date,
-        required:true,
-    },
-    end_date: {
-        type: Date
-    },
-    start_time:{
-        type:String,
-        required:true
-   },
-   end_time:{
-      type:String,
-      required:true,
-   },
-    createdAt:{
-        type: Date,
-        default: Date.now
-    },
-    updated_at: {
-        type: Date,
-        default: Date.now
-    },
-    deleted_at:{
-        type:Date
-    },
-    status:{
-        type:Number,
-        required:true,
-    },
-    highest_bid: {
-        type: Number,
-        required: true
-    },
-    highest_bid_user: {
-        type: Number,
-        required: true
-    },
-    reserve: {
-        type: Number,
-        default: null
-    },
-    commission: {
-        type: Number,
-        default: null
-    },
-    emd: {
-        type: Number,
-        default: null
-    },
-    min_increament: {
-        type: Number,
-        default: null
-    },
-    permitted_user: {
-        type: [String],
-        required: true
-    },
-    open_status: {
-        type: String,
-        enum: ['O', 'S'],
-        required: true
-    }
+    productName:{type:String,required:true},
+    referenceNumber: { type: String, required: true },
+    categories: { type: String, required: true },
+    auctionStartDate: { type: Date, required: true },
+    auctionStartTime: { type: String, required: true },
+    auctionEndDate: { type: Date, required: true },
+    auctionEndTime: { type: String, required: true },
+    basePrice: { type: Number, required: true },
+    reservePrice: { type: Number, required: true },
+    minIncrement: { type: Number, required: true },
+    EMD: { type: Number, required: true },
+    Commission: { type: Number, required: true },
+    startBid: { type: Number, required: true },
+    ownershipStatus: { type: String, required: true },
+    propertyType: { type: String, required: true },
+    HOT: { type: String, required: true },
+    OnlineorInPerson:{type:String,required:true},
+    Street:{type:String,required:true},
+    City:{type:String,required:true},
+    Country:{type:String,required:true},
+    ZipCode:{type:Number,required:true},
+    State:{type:String,required:true},
+    yearBuilt: { type: Number, required: true },
+    manufacturer: { type: String, required: true },
+    userProducts: { type: Number, required: true },
+    floor: { type: Number, required: true },
+    totalFloor: { type: Number, required: true },
+    totalArea: { type: Number, required: true },
+    bedroom: { type: Number, required: true },
+    bathroom: { type: Number, required: true },
+    squareFootage:{type:Number,required:true},
+    lotSize:{type:Number,required:true},
+    monthlyHodDues:{type:Number,required:true},
+    trusteeSaleNumber:{type:Number,required:true},
+    Apn:{type:Number,required:true},
+    eventId:{type:Number,required:true},
+    alternateEmails: [String],
+    productImage:{type:String,required:true},
+    productImages:  [
+        {
+            url: {
+                type: String,
+                required: true
+            }
+        }
+    ],
 })
 productSchema.pre('save', async function (next) {
     this.updated_at = Date.now();
