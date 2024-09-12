c1122c70cb52c0fea6580eec7d5e83bca3914781
Asad Rana <asadlukman246@gmail.com>
2024-09-12 21:03:27 +0530

    edits

--- controller/userController.js
+++ controller/userController.js
@@ -4,7 +4,7 @@ const Errorhandler = require("../utils/errorhandler");
 const sendToken = require("../utils/getToken");
 const productModel = require("../model/productModel");
 const sendEmail = require("../utils/sendEmail");
-const axios =require("axios");
+const axios = require("axios");
 exports.CreateUser = catchAsyncError(
   async (req, res) => {
     // Create a new user
@@ -30,7 +30,7 @@ exports.CreateUser = catchAsyncError(
             <style>
                 body { font-family: Arial, sans-serif; font-size: 14px; color: #333; }
                 .container { width: 90%; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #f4f4f4; border-radius: 8px; }
-                .button { background-color: #28a745; color: #ffffff; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 10px 0; cursor: pointer; border-radius: 5px; }
+                .button { background-color: #0384fb; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 10px 0; cursor: pointer; border-radius: 5px; }
                 .footer { font-size: 12px; color: #666; }
                 .header { font-size: 24px; color: #333; }
                 .welcome-text { font-size: 16px; line-height: 1.6; }
@@ -49,8 +49,8 @@ exports.CreateUser = catchAsyncError(
         </body>
         </html>`
       );
-      
-      sendToken(newUser, 201, res,response.data.Details);
+
+      sendToken(newUser, 201, res, response.data.Details);
     } catch (error) {
       res.status(500).json({ success: false, message: 'Error sending OTP', error: error.message });
     }
@@ -83,13 +83,13 @@ exports.Login = catchAsyncError(
     });
     if (!finduser) return next(new Errorhandler("Invalid Email", 400));
     const matchPassword = await finduser.comparePassword(password);
-    console.log(matchPassword,"password");
+    console.log(matchPassword, "password");
     if (!matchPassword) return next(new Errorhandler("Invalid Password", 400));
-     sendEmail(
-        req.body.email,
-        finduser.name,
-        "Welcome to Vihara",
-        `<!DOCTYPE html>
+    sendEmail(
+      req.body.email,
+      finduser.name,
+      "Welcome to Vihara",
+      `<!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
@@ -116,7 +116,7 @@ exports.Login = catchAsyncError(
             </div>
         </body>
         </html>`
-);
+    );
     sendToken(finduser, 200, res);
   }
 )
@@ -203,93 +203,214 @@ exports.getAllEmails = catchAsyncError(
     }
   }
 );
-exports.resetPassword=catchAsyncError(
-  async(req,res,next)=>{
-     console.log(req.params.token);
-      const resetPasswordToken=require("crypto").createHash("sha256").update(req.params.token).digest("hex");
-      const User=await userModel.findOne({resetPasswordToken,
-       resetPasswordExpire:{$gt:Date.now()}
-     })
-     if(!User){
-         return next(new Errorhandler("resetPassword token is invalid or has been expired",400));
-     }
-     User.password=req.body.password;
-     User.resetPasswordToken=undefined;
-     User.resetPasswordExpire=undefined;
-     await User.save({validateBeforeSave:false})
-     sendToken(User,200,res);
+exports.resetPassword = catchAsyncError(
+  async (req, res, next) => {
+    console.log(req.params.token);
+    const resetPasswordToken = require("crypto").createHash("sha256").update(req.params.token).digest("hex");
+    const User = await userModel.findOne({
+      resetPasswordToken,
+      resetPasswordExpire: { $gt: Date.now() }
+    })
+    if (!User) {
+      return next(new Errorhandler("resetPassword token is invalid or has been expired", 400));
+    }
+    User.password = req.body.password;
+    User.resetPasswordToken = undefined;
+    User.resetPasswordExpire = undefined;
+    await User.save({ validateBeforeSave: false })
+    sendToken(User, 200, res);
   }
 )
-exports.updatePassword=catchAsyncError(
-  async(req,res)=>{
-       const User=await userModel.findById(req.user.id).select("+password");
-       const isMatchPassword=User.comparePassword(req.body.oldpassword);
-       if(!isMatchPassword){
-         return next(new Errorhandler("OldPassword is wrong",400));
-       }
-       if(req.body.newpassword!==req.body.confirmpassword){
-         return next(new Errorhandler("Passwords not matching",400));
-       }
-       User.password=req.body.newpassword;
-       await User.save();
-       sendToken(User,200,res);
+exports.updatePassword = catchAsyncError(
+  async (req, res) => {
+    const User = await userModel.findById(req.user.id).select("+password");
+    const isMatchPassword = User.comparePassword(req.body.oldpassword);
+    if (!isMatchPassword) {
+      return next(new Errorhandler("OldPassword is wrong", 400));
+    }
+    if (req.body.newpassword !== req.body.confirmpassword) {
+      return next(new Errorhandler("Passwords not matching", 400));
+    }
+    User.password = req.body.newpassword;
+    await User.save();
+    sendToken(User, 200, res);
   }
 )
-exports.forgotPassword=catchAsyncError(
-  async(req,res,next)=>{
-      const {email}=req.body;
-      console.log(email)
-      const User=await userModel.findOne({email});
-      if(!User){
-        return next(new Errorhandler("No user found with this email",404));
-      }
-      const resetPasswordtoken=User.getResetPasswordToken();
-      await User.save({validateBeforeSave:false});
-      const resetUrl=`${process.env.CLIENT_URL}/set-new-password/${resetPasswordtoken}`; 
-      const message=`Your reset password token is:- /n/n ${resetUrl}, if You have not requested then please ignore it`
-      try{
-               sendEmail(
-                req.body.email,
-                User.name,
-                "Email password recovery",
-                `<!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Password Reset</title>
-                    <style>
-                        body { font-family: Arial, sans-serif; font-size: 14px; color: #333; }
-                        .container { width: 90%; max-width: 600px; margin: 0 auto; }
-                        .button { background-color: #007BFF; color: #ffffff; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 10px 0; cursor: pointer; border-radius: 5px; }
-                        .footer { font-size: 12px; color: #666; }
-                        .header { font-size: 20px; color: #333; }
-                    </style>
-                </head>
-                <body>
-                    <div class="container">
-                        <h1 class="header">Hi ${User.name},</h1>
-                        <p>We received your request to reset your password. Please click on the link below or copy and paste the URL into your browser.</p>
-                        <a href="${resetUrl}" class="button">Reset Password</a>
-                        <p>This URL is only valid for 30 days. If it expires, please visit our website.</p>
-                        <p>Your security is important to us. If you did not request to change your password, please ignore this email.</p>
-                        <p>Thank you,<br>The Vihara Team</p>
-                        <hr>
-                        <p class="footer">If you have any questions or need further assistance, please contact Customer Care at trisha@vihara.com.</p>
-                    </div>
-                </body>
-                </html>`
-                
-              )  
-              res.status(200).json({
-                success:true,
-                message:"Recovery Email sent to user"
-              })
-      }catch(error){
-          User.resetPasswordToken=undefined;
-          User.resetPasswordExpire=undefined;
-          await User.save({validation:false});
-          return next(new Errorhandler(error.message,500));
-      }
+exports.forgotPassword = catchAsyncError(
+  async (req, res, next) => {
+    const { email } = req.body;
+    console.log(email)
+    const User = await userModel.findOne({ email });
+    if (!User) {
+      return next(new Errorhandler("No user found with this email", 404));
+    }
+    const resetPasswordtoken = User.getResetPasswordToken();
+    await User.save({ validateBeforeSave: false });
+    const resetUrl = `${process.env.CLIENT_URL}/set-new-password/${resetPasswordtoken}`;
+    const message = `Your reset password token is:- /n/n ${resetUrl}, if You have not requested then please ignore it`
+    try {
+      sendEmail(
+        req.body.email,
+        User.name,
+        "Email password recovery",
+        `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <title>Password Reset</title>
+    <style>
+        body { 
+            font-family: Arial, sans-serif; 
+            font-size: 14px; 
+            color: black !important; 
+            margin: 0; 
+            padding: 0; 
+            background-color: #f4f4f4;
+        }
+            p{
+                color: black !important; 
+                font-size:16px;
+                font-weight:400;
+            }
+        .container { 
+            width: 95%; 
+            max-width: 600px; 
+            margin: 0 auto; 
+            background-color: #ffffff; 
+            padding: 20px; 
+        }
+        .header img {
+            max-width: 120px;
+            height:auto;
+            display: block;
+            margin:0 auto;
+        }
+        .main-content { 
+            text-align: left; 
+        }
+        .main-content h1 { 
+            font-size: 20px; 
+            margin: 20px 0;
+            color: #333333;
+        }
+        .button { 
+            background-color: #0384fb; 
+            color: #ffffff !important; 
+            padding: 10px 20px; 
+            text-align: center; 
+            text-decoration: none; 
+            display: inline-block; 
+            font-size: 16px; 
+            margin: 20px 0; 
+            cursor: pointer; 
+            border-radius: 5px;
+            display: block;
+            width: fit-content;
+            margin: 20px auto;
+        }
+        .footer { 
+            font-size: 12px; 
+            color: #666666; 
+            margin-top: 20px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+        .footer-logo{
+           width:70%;
+        }
+        .footer-logo img {
+            width: 120px;
+            height:auto;
+        }
+        .social-icons {
+            width:30%;
+            display: flex;
+            gap: 10px;
+        }
+        .social-icons a {
+            display: inline-block;
+            margin-right:10px;
+        }
+        .social-icons img {
+            width: 30px;
+            height: 30px;
+        }
+        .contact-info {
+            margin-top: 20px;
+            font-size: 12px;
+            color: #666666;
+        }
+        .contact-info a {
+            color: #007BFF;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <!-- Header -->
+        <div class="header">
+            <img src="https://www.vihara.ai/static/media/vihara-new-logo.1e0ecd4b8707813c361a.jpeg" alt="Company Logo">
+        </div>
+        
+        <!-- Main Content -->
+        <div class="main-content">
+            <p>Hi <strong>${User.name}</strong>,</p>
+            <p>We received your request to reset your password. Please click on the link below or copy and paste the URL into your browser.</p>
+            <a href="${resetUrl}" class="button">Reset Password</a>
+            <p>This reset URL is only valid for 30 days. If it expires, please visit our website, sign in, and request a new password reset link.</p>
+            <p>Your security is important to us. If you did not request to change your password, please ignore this email.</p>
+        </div>
+      <div class="main-content">
+         <p>Thank You,
+            <br/>
+            The Vihara Team
+         </p>
+      </div>
+        <!-- Footer -->
+     <div class="footer">
+            <div class="footer-logo">
+                <img src="https://www.vihara.ai/static/media/vihara-new-logo.1e0ecd4b8707813c361a.jpeg" alt="Company Logo" style="height: 40px;">
+            </div>
+            <div class="social-icons" style="gap:10px;">
+                <a href="https://www.facebook.com">
+                    <img src="https://ci3.googleusercontent.com/meips/ADKq_NYdCsK3DhV8iuJ7KDeQ4CZlaIT8Sd8dS-bL2M_9-lUDE--JPQiQHWc_FtdACnIkeFGtKIbrGuhcEFm3yManRJvDtAUz4D3UsqveC8gPdvJ0sA6EiRJURxk2OSEQxx55L7GHtY2RGINW87DbXx7fjVn0a26tV6c3BlnUTvrNCriS=s0-d-e1-ft#https://image.s11.sfmc-content.com/lib/fe3a11717164047d7d1572/m/1/e8f22763-dc43-4959-b3e9-5a8048b273a1.png" alt="Facebook">
+                </a>
+                <a href="https://www.twitter.com">
+                    <img src="https://ci3.googleusercontent.com/meips/ADKq_Na1EsBWS9vfqdVpS9-mHalzjVCy6XyghIY8kjzp3PGKy-0rDx8O4peoE0epBbkg-qsA_qBPcMfSlcRNKE6rfM9FsErlgkJv5FA87ZmwoZA2q9Z46IrX6v3sWBWM_TBy87XvnCFu6gtcVtr6KumY932kiquePdG1xBkIZBdXFhG0=s0-d-e1-ft#https://image.s11.sfmc-content.com/lib/fe3a11717164047d7d1572/m/1/b67ecf84-4efa-431c-9b78-000164cf247c.png" alt="Twitter">
+                </a>
+                <a href="https://www.linkedin.com">
+                    <img src="https://ci3.googleusercontent.com/meips/ADKq_NZSsc060crzcO_DBbMfwxA4yCle_n0m1ONl46fk_voetKez4PGuHkMsVkv3L-l8qI7jrNg32izm6m0AA2f3xg1-Eq6Hm0h2Mx01uw0ZCZb2pUYQYnyVVK6IGq0LGbfQY72fKk72zjySmaMB5ZTDw9YebJld3-NA1tCX03toHpuU=s0-d-e1-ft#https://image.s11.sfmc-content.com/lib/fe3a11717164047d7d1572/m/1/0ac84e3b-25d2-4bf7-81f1-740f7bfdc8c5.png" alt="LinkedIn">
+                </a>
+                <a href="https://www.youtube.com">
+                    <img src="https://ci3.googleusercontent.com/meips/ADKq_Na43exfG770dgwf_6ah7ag4uFwZIVqzRfY3ER8T10aiIKRL6SUETzkHDIX2AI2O0oLDzwjnSoPWeomEVNdxGvpN21CfTQOdX6M35KCM7brGLptGqjG1-v0ju2BwoL_GSF7OG3D1OeV-R0sm29obAfJ7gtp4Bwz05MZieQ2rWRes=s0-d-e1-ft#https://image.s11.sfmc-content.com/lib/fe3a11717164047d7d1572/m/1/e2ae1bd7-6b6f-4608-9aff-88a06973551f.png" alt="YouTube">
+                </a>
+            </div>
+        </div>
+
+        <!-- Contact Info -->
+        <div class="contact-info">
+            <p>If you have any questions or need assistance, please contact our support team at <a href="mailto:trisha@vihara.com">trisha@vihara.com</a>.</p>
+            <p>Altisource Online Auction, Inc.<br>2300 Lakeview Parkway, 7th Floor, Space 756, Alpharetta, GA 30009</p>
+        </div>
+    </div>
+</body>
+</html>
+`
+
+      )
+      res.status(200).json({
+        success: true,
+        message: "Recovery Email sent to user"
+      })
+    } catch (error) {
+      User.resetPasswordToken = undefined;
+      User.resetPasswordExpire = undefined;
+      await User.save({ validation: false });
+      return next(new Errorhandler(error.message, 500));
+    }
   }
 )
