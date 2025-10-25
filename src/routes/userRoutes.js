a986c00d546aaf254bde3763c0f284783c2b3df8
Asad Rana <asadlukman246@gmail.com>
2025-10-25 16:53:09 +0530

    add auth middleware in user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -9,14 +9,14 @@ const router = express.Router();
 router.post("/registerUser", CreateUser);
 router.post("/login", Login);
 router.get("/logout", LogOut);
-router.put("/save-property", saveProperty);
-router.put("/remove-property", removeProperty);
-router.get("/save-property/get", allsavedProperties);
+router.put("/save-property",isAuthenticated, saveProperty);
+router.put("/remove-property",isAuthenticated, removeProperty);
+router.get("/save-property/get", isAuthenticated,allsavedProperties);
 router.get("/get", isAuthenticated, getUser)
 router.get("/getEmails", getAllEmailandPhone);
 router.post("/forgot/password", forgotPassword);
 router.post("/password/reset/:token", resetPassword);
-router.put('/update/:userId', updateUserDetails);
+router.put('/update/:userId',isAuthenticated, updateUserDetails);
 router.post('/send-otp', sendOTP);
 router.post('/send-sms-otp', sendSmSOTP);
 router.post('/recaptcha', recaptcha);
